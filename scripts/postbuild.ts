4262a87db7966fd551ac3a990e3bbbb9f6a6ec44
오승환 <oshxios@gmail.com>
2020-05-03 16:26:13 +0900

    나름 배포 설정 추가

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,20 +1,21 @@
-import SimpleGIT from 'simple-git'
 import rimraf from 'rimraf'
+// @ts-ignore
+import simpleGit from 'simple-git'
 
-const SSH_GIT_URL = process.env.npm_package_config_ssh
-const REMOTE = process.env.npm_package_config_remote
-const BRANCH = process.env.npm_package_config_branch
+const SSH_GIT_URL = process.env.npm_package_config_ssh as string
+const REMOTE = process.env.npm_package_config_remote as string
+const BRANCH = process.env.npm_package_config_branch as string
 
 const info = (logName: string) => (...args: any[]) =>
   console.debug('git', logName, args?.[0] === null ? 'succeed.' : args)
 
 rimraf('./dist/.git', (): void => {
   console.debug('[./dist/.git] has been removed.')
-  SimpleGIT('./dist')
+  ;(simpleGit('./dist')
     .init(false, info('[init]'))
     .checkoutLocalBranch(BRANCH, info('[checkout]'))
     .addRemote(REMOTE, SSH_GIT_URL, info('[addRemote]'))
-    .fetch(REMOTE, BRANCH, info('[fetch]'))
+    .fetch(REMOTE, BRANCH, info('[fetch]')) as any)
     .raw(['reset', '--mixed', `${REMOTE}/${BRANCH}`], info('[raw]'))
     .add('*', info('[add]'))
     .commit(`[AUTO-BUILD] ${new Date()}`, info('[commit]'))
